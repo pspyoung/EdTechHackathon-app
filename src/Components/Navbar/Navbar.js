782a39cc0dd7c9144f7925d23213dda35fdd2740
pspyoung <pspyoung2@gmail.com>
2022-05-16 14:50:02 -0700

    Adds Navbar stylings

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import './Navbar';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar(props) {
 	return (
-		<div>
-			<Link to="/">
-				<h4>Go Home</h4>
-			</Link>
-			<Link to="/Assessments">
-				<h4>Assessments</h4>
-			</Link>
-			<Link to="/Meditate">
-				<h4>Meditate</h4>
-			</Link>
-			<Link to="/Timer">
-				<h4>Timer</h4>
-			</Link>
-			<Link to="/User">
-				<h4>User</h4>
-			</Link>
-		</div>
+		<nav className="navBar-container">
+			<div className="navBar">
+				<Link to="/">
+					<h4>Go Home</h4>
+				</Link>
+				<Link to="/Assessments">
+					<h4>Assessments</h4>
+				</Link>
+				<Link to="/Meditate">
+					<h4>Meditate</h4>
+				</Link>
+				<Link to="/Timer">
+					<h4>Timer</h4>
+				</Link>
+				<Link to="/User">
+					<h4>User</h4>
+				</Link>
+			</div>
+		</nav>
 	);
 }
 
